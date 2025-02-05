3782afa51c2e987081062a32d75cd529f535d664
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2025-02-05 16:56:17 -0300

    chore: create basic navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>HOME</Text>
+      <Link href="/about" style={styles.button}>Go to About screen</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292E",
+  },
+  text: {
+    color: "#FFF",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#FFF",
+  },
+});
