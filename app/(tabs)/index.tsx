1350f6afa57caf4fca7cb62f6722243051fa3879
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2025-02-06 17:31:07 -0300

    chore: use expo image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,19 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <View>
+        <Image source={PlaceholderImage}/>
+      </View>
       <Text style={styles.text}>HOME</Text>
-      <Link href="/about" style={styles.button}>Go to About screen</Link>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -16,6 +24,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#25292E",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
   text: {
     color: "#FFF",
